814487a469ffff22f80bc176f094b4722d32a28a
csenzi2mo-collab <csenzi2mo@gmail.com>
2026-09-02 17:23:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
+const { route } = require('../server');
+
+module.exports = async (req, res) => {
+  try {
+    await route(req, res);
+  } catch (err) {
+    console.error(err);
+    if (!res.headersSent) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      res.end(JSON.stringify({ error: { message: 'Internal server error' } }));
+    }
+  }
+};
 
